6e72234ec768ee12c0ff62cc915ff109b9ab20ff
zackzheng <zhengzuanzhe@gmail.com>
2022-07-20 22:40:55 +0800

    first commit

--- Location/ViewController.swift
+++ Location/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        let currentLocation = MKMapItem.forCurrentLocation()
+//        MKMapItem.openMaps(with: [currentLocation])
+        
+//        CLLocationCoordinate2D loc = CLLocationCoordinate2DMake([self.model.latitude floatValue], [self.model.longitude floatValue]);
+//
+//            MKMapItem *toLocation = [[MKMapItem alloc] initWithPlacemark:[[MKPlacemark alloc] initWithCoordinate:loc addressDictionary:nil]];
+//            [MKMapItem openMapsWithItems:@[currentLocation, toLocation]
+//                           launchOptions:@{MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving,
+//                                           MKLaunchOptionsShowsTrafficKey: [NSNumber numberWithBool:YES]}];
     }
-
-
 }
-
